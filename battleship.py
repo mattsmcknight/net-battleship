f750f9b120a024cd4342a9a78736f9c86828e524
Matt McKnight <mattsmcknight@gmail.com>
2017-12-22 18:15:22 -0500

    Place Pieces Function

--- battleship.py
+++ battleship.py
@@ -36,6 +36,8 @@ def split_order(b):
     b = b.split(',')
     if len(b) == 1:
         return False
+    if b[2] == 'Winner!'
+        return (int(b[0]), b[1]), b[2]
     b[2] = b[2] == 'True'
     return (int(b[0]), b[1]), str(b[2])
 
